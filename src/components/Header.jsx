cc47edb7e4b9bc4a8441380f4bfc2bcaee941ad4
PaoloFerrari92 <paolo.as92@gmail.com>
2023-01-24 20:58:47 +0100

    Aggiunta seconda sezione Header con icone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import {BsSearch} from 'react-icons/bs';
 
 const Header = () => {
   return (
@@ -9,12 +10,74 @@ const Header = () => {
           <div className="row">
             <div className="col-6">
               <p className='text-white mb-0'>
-                 Free Shipping Over 100€ & Free Returns</p>
+                Free Shipping Over 100€ & Free Returns</p>
             </div>
             <div className="col-6">
               <p className='text-end text-white mb-0'>
-                 Hotline: 
-                 <a className='text-white' href="tel:+39 349 555 9765">+39 349 555 9765</a></p>
+                Hotline:
+                <a className='text-white' href="tel:+39 349 555 9765">+39 349 555 9765</a></p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className='header-upper py-3'>
+        <div className="container-xxl">
+          <div className='row align-items-center'>
+            <div className='col-2'>
+              <h2>
+                <Link className='text-white'>Developers</Link>
+              </h2>
+            </div>
+            <div className='col-5'>
+              <div className="input-group ">
+                <input 
+                type="text" 
+                className="form-control py-2" 
+                placeholder="Search Product Here"
+                aria-label="Search Product Here" 
+                aria-describedby="basic-addon2"
+                 />
+                  <span className="input-group-text p-3" id="basic-addon2">
+                    <BsSearch className='fs-6'/>
+                  </span>
+              </div>
+            </div>
+            <div className='col-5'>
+              <div className='header-upper-links d-flex align-items-center justify-content-between'>
+                <div>
+                <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/compare.svg' alt='compare' />
+                    <p className='mb-0'>
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                <Link className='d-flex align-items-center gap-10 text-white'>
+                <img src='images/wishlist.svg' alt='wishlist' />
+                    <p className='mb-0'>
+                      Favorite <br /> wishlist
+                    </p>
+                </Link>
+                </div>
+                <div>
+                <Link className='d-flex align-items-center gap-10 text-white'>
+                <img src='images/user.svg' alt='user' />
+                    <p className='mb-0'>
+                      Login <br /> My Account
+                    </p>
+                </Link>
+                </div>
+                <div>
+                <Link className='d-flex align-items-center gap-10 text-white'>
+                <img src='images/cart.svg' alt='cart' />
+                   <div className='d-flex flex-column gap-10'>
+                      <span className='badge bg-white text-dark'>0</span>
+                      <p className='mb-0'> 300,00 €</p>
+                   </div>
+                </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
